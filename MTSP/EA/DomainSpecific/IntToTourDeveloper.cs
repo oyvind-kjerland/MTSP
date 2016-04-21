e010601c660fff4a5dacac688d149c32ae82d82b
Øyvind <oyvindk_93@hotmail.com>
2016-04-21 16:07:10 +0200

    Add phenotype and developer for TourPhenotype

---
+++ MTSP/EA/DomainSpecific/IntToTourDeveloper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTSP.EA.DomainSpecific
+{
+    class IntToTourDeveloper : PhenotypeDeveloper
+    {
+        public override Phenotype Develop(Genotype genotype)
+        {
+            IntGenotype intGenotype = (IntGenotype)genotype;
+            TourPhenotype tourPhenotype = new TourPhenotype();
+
+            int max = intGenotype.Max;
+            int[] tour = new int[max];
+
+            tourPhenotype.Tour = tour;
+
+            // Fill in possible cities
+            List<int> possibleCities = new List<int>(max);
+            for (int i = 0; i < max; i++)
+            {
+                possibleCities[i] = i;
+            }
+
+            
+            // Choose cities based on genotype
+            int c;
+            for (int i = 0; i < max; i++)
+            {
+                c = i % possibleCities.Count;
+                tour[i] = possibleCities[c];
+                possibleCities.RemoveAt(c);
+            }
+
+            return tourPhenotype;
+        }
+    }
+}
---
+++ MTSP/EA/DomainSpecific/TourPhenotype.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTSP.EA.DomainSpecific
+{
+    class TourPhenotype : Phenotype
+    {
+
+        public int[] Tour { get; set; }
+
+
+        public override string GetPhenotypeString()
+        {
+            string s = "";
+            for (int i=0; i<Tour.Length; i++)
+            {
+                s += Tour[i].ToString() + ",";
+            }
+            return s;
+        }
+    }
+}
