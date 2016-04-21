307c54533e5300e72bc5dfbf33d2473eb556dbe5
Øyvind <oyvindk_93@hotmail.com>
2016-04-21 16:51:04 +0200

    Add CityData class for loading data from csv files

---
+++ MTSP/CityData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MTSP
+{
+    class CityData
+    {
+
+        public int Length = 48;
+
+        public int[,] DistanceArr { get; private set; }
+        public int[,] CostArr { get; private set; }
+
+        public CityData()
+        {
+            DistanceArr = ReadCSV("Distance.csv");
+            CostArr = ReadCSV("Cost.csv");
+
+        }
+
+        // Read csv files and parse them
+        private int[,] ReadCSV(string filename)
+        {
+
+            int[,] arr = new int[Length, Length];
+
+            StreamReader reader = new StreamReader(File.OpenRead(filename));
+
+            int i = 0;
+            int val = 0;
+            string empty = "";
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split(',');
+                for (int j=0; j<i+1; j++)
+                {
+                    if (values[j] == empty) continue;
+                    val = int.Parse(values[j]);
+                    arr[i, j] = val;
+                    arr[j, i] = val;
+                }
+                i++;
+            }
+            reader.Close();
+
+            return arr;
+        
+        }
+
+    }
+}
