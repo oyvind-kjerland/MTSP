a11db6fbd2853a380d693ab1cbbece97afc7aba7
Øyvind <oyvindk_93@hotmail.com>
2016-04-21 15:50:56 +0200

    Add IntGenotype with int genes

---
+++ MTSP/EA/DomainSpecific/IntGenotype.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTSP.EA.DomainSpecific
+{
+    class IntGenotype : Genotype
+    {
+        public int[] List { get; set; }
+        public int Length { get; private set; }
+        public int Min;
+        public int Max;
+
+        public IntGenotype(int length)
+        {
+            Length = length;
+            List = new int[length];
+        }
+
+        public IntGenotype(int length, int[] list)
+        {
+            List = list;
+        }
+
+        public override Genotype Clone()
+        {
+            return new IntGenotype(Length, (int[])List.Clone());
+        }
+
+        public override void Randomize(Random random)
+        {
+            for (int i=0; i<Length; i++)
+            {
+                List[i] = random.Next(Min, Max);
+            }
+        }
+    }
+}
